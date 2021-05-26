2ff05e8bad01ce185c4f26ebb463ca99bebbc797
Dylan <dcren2010@gmail.com>
2021-05-26 11:25:46 -0400

    add soldout indicator styles

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
 
+  # For showing all products
   def index
     @products = Product.all.order(created_at: :desc)
   end
 
+  # For displaying single item
   def show
     @product = Product.find params[:id]
   end

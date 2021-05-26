df26bb2567838172d0450783e3bb52ac90990b64
Dylan <dcren2010@gmail.com>
2021-05-26 16:34:26 -0400

    add Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < Admin::BaseAdminController
   def show
+    @products = Product.all
+    @product_count = Product.sum('quantity')
+    @categories = Category.all
+    @category_count = Category.count
+
   end
 end
